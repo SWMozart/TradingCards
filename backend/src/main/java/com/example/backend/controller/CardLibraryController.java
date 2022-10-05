6cae028ee98efbed4020ea5f992f98fa84fe0195
hunjoonrhee <hunjoonrhee90@gmail.com>
2022-10-05 14:44:16 +0200

    BE: create first Card in mongo db

--- backend/src/main/java/com/example/backend/controller/CardLibraryController.java
+++ backend/src/main/java/com/example/backend/controller/CardLibraryController.java
@@ -1,4 +1,27 @@
 package com.example.backend.controller;
 
+import com.example.backend.model.Card;
+import com.example.backend.service.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/cards")
 public class CardLibraryController {
+    private CardService cardService;
+
+    @Autowired
+    public CardLibraryController(CardService cardService) {
+        this.cardService = cardService;
+    }
+
+    @PostMapping
+    public Card addCard(@RequestBody Card card){
+        return cardService.addCard(card);
+    }
 }
--- backend/src/main/java/com/example/backend/controller/PacksController.java
+++ backend/src/main/java/com/example/backend/controller/PacksController.java
@@ -1,8 +1,7 @@
 package com.example.backend.controller;
 
-import com.example.backend.service.PacksService;
+import com.example.backend.service.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/packs")
 public class PacksController {
 
-    private PacksService packsService;
+    private CardService cardService;
 
     @Autowired
-    public PacksController(PacksService packsService) {
-        this.packsService = packsService;
+    public PacksController(CardService cardService) {
+        this.cardService = cardService;
     }
 
 
