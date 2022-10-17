1fe3cfe0093db5629b6bdea77ca1ec97043164de
Nina Guo <ninaguo1305@gmail.com>
2022-10-17 21:03:16 +0800

    BE: Add PackControllertest and PackServicetest

---
+++ backend/src/test/java/com/example/backend/controller/PackControllerTest.java
@@ -0,0 +1,139 @@
+package com.example.backend.controller;
+
+import com.example.backend.model.Card;
+import com.example.backend.model.Pack;
+import com.example.backend.repository.PackRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PackControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private  PackRepo packrepo;
+
+
+
+    @Test
+    void getAllPacks() throws Exception {
+        //GIVEN
+        Card dummyCard = new Card("1","test1","test2","test3","test4");
+        Card dummyCard2 = new Card("2","test1","test2","test3","test4");
+        Card dummyCard3 = new Card("3","test1","test2","test3","test4");
+
+
+        Pack dummyPack = new Pack("1","Legend Player1","Pack1", List.of(dummyCard,dummyCard2,dummyCard3));
+        packrepo.save(dummyPack);
+
+
+        // WHEN & THEN
+
+        String expectedJSON = """
+            [
+               {
+                   "id": "1",
+                   "category": "Legend Player1",
+                   "name": "Pack1",
+                   "cardsInAPack": [
+                       {
+                           "id": "1",
+                           "name": "test1",
+                           "position": "test2",
+                           "category": "test3",
+                           "poster": "test4"
+                       },
+                       {
+                           "id": "2",
+                           "name": "test1",
+                           "position": "test2",
+                           "category": "test3",
+                           "poster": "test4"
+                           },
+                       {
+                           "id": "3",
+                           "name": "test1",
+                           "position": "test2",
+                           "category": "test3",
+                           "poster": "test4"
+                       }
+                   ]
+               }
+               ]
+                """;
+        mockMvc.perform(get("/api/packs"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(expectedJSON));
+    }
+
+
+
+    @Test
+    void addANewPack() throws Exception{
+
+        String requestBody = """
+                        {
+                        "id": "test",
+                        "name": "pack1",
+                        "category":"Legend Player1",
+                        "cardInAPack":"1","2", "3"
+                        }
+                """;
+        String expectedJSON = """
+            [
+               {
+                   "id": "1",
+                   "category": "Legend Player1",
+                   "name": "Pack1",
+                   "cardsInAPack": [
+                       {
+                           "id": "1",
+                           "name": "test1",
+                           "position": "test2",
+                           "category": "test3",
+                           "poster": "test4"
+                       },
+                       {
+                           "id": "2",
+                           "name": "test1",
+                           "position": "test2",
+                           "category": "test3",
+                           "poster": "test4"
+                           },
+                       {
+                           "id": "3",
+                           "name": "test1",
+                           "position": "test2",
+                           "category": "test3",
+                           "poster": "test4"
+                       }
+                   ]
+               }
+               ]
+                """;
+
+        // WHEN THEN
+        mockMvc.perform(post("/api/packs")
+                                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                                .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(expectedJSON));
+
+
+
+    }
+
+}
---
+++ backend/src/test/java/com/example/backend/service/PackServiceTest.java
@@ -0,0 +1,51 @@
+package com.example.backend.service;
+
+import com.example.backend.model.Card;
+import com.example.backend.model.Pack;
+import com.example.backend.repository.CardRepo;
+import com.example.backend.repository.PackRepo;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+class PackServiceTest {
+
+   private final PackRepo repo =mock(PackRepo.class);
+
+    private final IdService idService = mock(IdService.class);
+    private final CardRepo cardRepo = mock (CardRepo.class);
+
+    private final PackService service = new PackService(repo, cardRepo, idService);
+
+    @Test
+    void getAllPacks() {
+      //GIVEN
+
+        Card dummyCard = new Card("1","test1","test2","test3","test4");
+        Card dummyCard2 = new Card("2","test1","test2","test3","test4");
+        Card dummyCard3 = new Card("3","test1","test2","test3","test4");
+
+        Pack pack = new Pack("1","Legend Player1","Pack1", List.of(dummyCard,dummyCard2,dummyCard3));
+        when(idService.generateId()).thenReturn("1");
+
+        when(repo.findAll())
+                .thenReturn(List.of(pack));
+
+        //WHEN
+        List<Pack> actual = service.getAllPacks();
+
+        //Then
+        List<Pack> expected = List.of(pack);
+        assertEquals(expected, actual);
+    }
+
+
+
+}
